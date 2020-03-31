d93a4e032862b5d58fc1bbc62b8fc5631585becf
Beheerder Broekhuizen-Versteeg <manZ130194@umcn.nl>
2020-03-31 11:21:10 +0200

    setup2

--- scenario1.R
+++ scenario1.R
@@ -1,11 +1,12 @@
-n.simulation.runs <- 10
-n.people <- 1000 # persons in simulation
-n.days <- 31 # number of days that will be simulated
+n.simulation.runs <- 1e3
+n.people <- 1500 # persons in simulation
+n.days <- 365 # number of days that will be simulated
 n.buildings <- 3 # buildings in the area, people are split between these
-outside.connection <- c(200, 400) # these many people enter and leave every day
-detection.probability <- 0.8 # if one has corona, how likely to be detected and subsequently go home?
+outside.connection <- c(20, 40) # these many people enter and leave every day
+detection.probability <- .8 # if one has corona, how likely to be detected and subsequently go home?
 risky.time.building <- 1/20 # time (in hours) that people risk interacting with others in their building while arriving, going for lunch, or leaving
-base.probability.contraction <- .75 # per contact, how likely is an infected person to infect another?
+base.probability.contraction <- .9 # per contact, how likely is an infected person to infect another?
+social.distancing.proportion <- .25 # which percentage of people try to practice social distancing?
 social.distancing.effectiveness <- .8 # in each interaction, how likely is social distancing to work?
 arrival.range <- c(7, 8) # time that people arrive in the morning (in hours eg 12 is noon)
 lunch.range <- c(12, 13.5) # time that people leave the building for lunch (in hours eg 12 is noon)
@@ -13,12 +14,11 @@ leave.range <- c(17, 18.5) # time that people leave the building (in hours eg 12
 
 # setup data frame that contains person data
 people <- data.frame(
-  id = 1:n.people,
   age = runif(n.people, 19, 65),
   behaviour = sample(c("nolunch", "lunch"), n.people, T, c(2, 1)),
   corona.status = sample(c(FALSE, TRUE), n.people, T, c(9, 1)),
   detected = FALSE,
-  social.distancing = sample(c(FALSE, TRUE), n.people, T, c(1, 3)),
+  social.distancing = sample(c(FALSE, TRUE), n.people, T, c(1 - social.distancing.proportion, social.distancing.proportion)),
   works.in.building = sample(1:n.buildings, n.people, T),
   arrival.time = NA,
   lunch.time = NA,
@@ -28,6 +28,7 @@ people <- data.frame(
 # what we will collect for plotting at the end
 prevalence.overall <- prevalence <- matrix(0, nrow = n.buildings, ncol = n.days)
 
+detected.overall <- rep(0, n.days)
 ICU.load <- rep(0, n.days)
 
 for(run in 1:n.simulation.runs){
@@ -47,6 +48,7 @@ for(run in 1:n.simulation.runs){
     # people are detected
     detected <- which(people$corona.status == TRUE & runif(nrow(people)) < detection.probability)
     if(length(detected) > 0){
+      detected.overall[day] <- detected.overall[day] + length(detected)
       message(length(detected)," patients detected and quarantined.")
       people <- people[-detected, ]
     }
@@ -88,20 +90,22 @@ for(run in 1:n.simulation.runs){
     
     # people enter. every day, more corona arrives
     n.entering <- runif(1, outside.connection[1], outside.connection[2])
+    new.people <- data.frame(
+      age = runif(n.entering, 19, 65),
+      behaviour = sample(c("nolunch", "lunch"), n.entering, T, c(2, 1)),
+      corona.status = FALSE,
+      detected = FALSE,
+      social.distancing = sample(c(FALSE, TRUE), n.entering, T, c(1 - social.distancing.proportion, social.distancing.proportion)),
+      works.in.building = sample(1:n.buildings, n.entering, T),
+      arrival.time = NA,
+      lunch.time = NA,
+      leave.time = NA
+    )
+    cases.in.enterings <- min(round(day / 10), nrow(new.people))
+    new.people$corona.status[1:cases.in.enterings] <- TRUE
     people <- rbind(
       people,
-      data.frame(
-        id = (max(people$id) + 1):(max(people$id) + n.entering),
-        age = runif(n.entering, 19, 65),
-        behaviour = sample(c("nolunch", "lunch"), n.entering, T, c(2, 1)),
-        corona.status = c(rep(TRUE, day), rep(FALSE, n.entering - day)),
-        detected = FALSE,
-        social.distancing = sample(c(FALSE, TRUE), n.entering, T, c(1, 3)),
-        works.in.building = sample(1:n.buildings, n.entering, T),
-        arrival.time = NA,
-        lunch.time = NA,
-        leave.time = NA
-      )
+      new.people
     )
     
     # calculate metrics
@@ -117,7 +121,11 @@ for(run in 1:n.simulation.runs){
 
 prevalence.overall <- prevalence.overall / n.simulation.runs
 
-plot(prevalence.overall[1, ], type="l", col=rainbow(n.buildings)[1], ylim=0:1)
+
+par(mfcol=c(1, 2))
+plot(prevalence.overall[1, ],tlab="% infected", xlab="day", type="l", col=rainbow(n.buildings)[1], ylim=c(0, max(prevalence.overall)), lwd=3,main="Prevalence per building")
 for(i in 2:n.buildings){
-  lines(prevalence.overall[i, ], col=rainbow(n.buildings)[i])
+  lines(prevalence.overall[i, ], col=rainbow(n.buildings)[i], lwd=3)
 }
+plot(cumsum(detected.overall/n.simulation.runs), type = "l", main="Cumulative detected cases", lwd=3, xlab="day", ylab="cases")
+par(mfcol=c(1, 1))
